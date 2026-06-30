30c221bcba21010446f82d96bf855c6418197705
Derek Veit <derek.veit>
2026-06-30 13:20:03 -0700

    test_json_loader: update for revised data.

--- tests/test_json_loader.cpp
+++ tests/test_json_loader.cpp
@@ -4,7 +4,8 @@
 
 TEST_CASE("load_bodies reads bundled planet data", "[json]") {
     const auto bodies = solar::core::load_bodies("assets/data/bodies.json");
-    REQUIRE(bodies.size() == 4);
-    CHECK(bodies[1].name == "Earth");
-    CHECK(bodies[1].elements.semi_major_axis_km > 149000000.0);
+    REQUIRE(bodies.size() == 5);
+    CHECK(bodies[0].name == "Sun");
+    CHECK(bodies[2].name == "Earth");
+    CHECK(bodies[2].elements.semi_major_axis_km > 149000000.0);
 }
