be07b00fe8b2ce5165d339a8ec3fbbacd56e77e3
Derek Veit <derek.veit>
2026-07-08 04:33:48 -0700

    Color: add to_string.

--- src/app/color.hpp
+++ src/app/color.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "fmt/format.h"
+#include <string>
 namespace solar::app {
 
 struct Color {
@@ -7,6 +9,10 @@ struct Color {
     float g;
     float b;
     float a;
+
+    [[nodiscard]] std::string to_string() const {
+        return fmt::format("{:.02f} {:.02f} {:.02f} {:.02f}", r, g, b, a);
+    }
 };
 
 Color color_from_angle(double angle);
