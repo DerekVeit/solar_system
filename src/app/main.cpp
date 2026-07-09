b9a57d02a433c2fac96a235b301b184c8f97109f
Derek Veit <derek.veit>
2026-07-09 16:28:47 -0700

    main: minor cleanup.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,19 +3,12 @@
 
 #include <fmt/core.h>
 
-namespace {
-
-using solar::app::log;
-
-} // namespace
-
 int main() {
-    log("----------------------------------------");
+    solar::app::log("----------------------------------------");
     try {
         solar::app::AppObjects app = solar::app::make_app();
         solar::app::run_loop(app.context);
         solar::app::log_shutdown_report(app.simulation);
-
         return 0;
     } catch (const std::exception& error) {
         fmt::print(stderr, "fatal error: {}\n", error.what());
