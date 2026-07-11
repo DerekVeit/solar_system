55d55f77aaee8fbe71b9cee8af1235d5e714dddb
Derek Veit <derek.veit>
2026-07-11 05:06:46 -0700

    .clang-format: add "AlwaysBreakTemplateDeclarations: Yes".

--- src/app/logging.hpp
+++ src/app/logging.hpp
@@ -16,7 +16,8 @@ void log_line(std::string line);
 } // namespace detail
 
 /// Append one timestamped line to the log file (flushed for tail -f).
-template <typename... T> void log(fmt::format_string<T...> fmt_str, T&&... args) {
+template <typename... T>
+void log(fmt::format_string<T...> fmt_str, T&&... args) {
     const auto now = std::chrono::system_clock::now();
     detail::log_line(fmt::format("{:%Y-%m-%d %H:%M:%S} {}\n", now,
                                  fmt::format(fmt_str, std::forward<T>(args)...)));
